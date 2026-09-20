0738c7bf604df2b6ab5f86f08b67100495861208
Daniel Mbazu <Mbazudaniel97@gmail.com>
2026-09-20 16:49:05 +0000

    feat: register sales module

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -10,7 +10,9 @@ import { InvitationModule } from "./modules/invitation/invitation.module";
 import { ChannelModule } from "./modules/channel/channel.module";
 import { TelegramModule } from "./modules/telegram/telegram.module";
 import { WhatsAppModule } from "./modules/whatsapp/whatsapp.module";
+import { BusinessModule } from "./modules/business/business.module";
 import { ConversationalModule } from "./modules/conversational/conversational.module";
+import { SalesModule } from "./modules/sales/sales.module";
 
 @Module({
   imports: [
@@ -26,6 +28,7 @@ import { ConversationalModule } from "./modules/conversational/conversational.mo
     TelegramModule,
     WhatsAppModule,
     BusinessModule,
+    SalesModule,
     ConversationalModule,
   ],
 })
