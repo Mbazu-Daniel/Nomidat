50a3c8d079ec0c9fb795652883935b49cec9ca3e
Daniel Mbazu <Mbazudaniel97@gmail.com>
2026-09-20 15:13:06 +0000

    feat(mvp): add conversational business assistant

---
+++ apps/api/src/modules/conversational/conversational.module.ts
@@ -0,0 +1,10 @@
+import { Module } from "@nestjs/common";
+import { DbModule } from "../../common/db/db.module";
+import { ConversationalService } from "./conversational.service";
+
+@Module({
+  imports: [DbModule],
+  providers: [ConversationalService],
+  exports: [ConversationalService],
+})
+export class ConversationalModule {}
