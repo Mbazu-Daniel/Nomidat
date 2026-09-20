bd23c109c0427bdd8894bb8e0cfebd4c7c3df1f7
Daniel Mbazu <Mbazudaniel97@gmail.com>
2026-09-20 15:44:07 +0000

    feat(web): add business management route

---
+++ apps/web/src/routes/customers.tsx
@@ -0,0 +1,8 @@
+import { createFileRoute } from "@tanstack/react-router";
+import { BusinessListPage } from "@/components/nomidat/business-list-page";
+
+export const Route = createFileRoute("/customers")({ component: CustomersPage });
+
+function CustomersPage() {
+  return <BusinessListPage section="customers" />;
+}
