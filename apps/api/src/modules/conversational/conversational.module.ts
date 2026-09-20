dcc9831662abb98b62b37bbfade5a07c04541570
Daniel Mbazu <Mbazudaniel97@gmail.com>
2026-09-20 20:34:21 +0000

    feat: connect conversational reports

--- apps/api/src/modules/conversational/conversational.module.ts
+++ apps/api/src/modules/conversational/conversational.module.ts
@@ -1,9 +1,10 @@
 import { Module } from "@nestjs/common";
 import { DbModule } from "../../common/db/db.module";
+import { ReportsModule } from "../reports/reports.module";
 import { ConversationalService } from "./conversational.service";
 
 @Module({
-  imports: [DbModule],
+  imports: [DbModule, ReportsModule],
   providers: [ConversationalService],
   exports: [ConversationalService],
 })
