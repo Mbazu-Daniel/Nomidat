32489720c8284b0f234b1c4fbaee977150499f91
Daniel Mbazu <Mbazudaniel97@gmail.com>
2026-09-20 20:06:26 +0000

    fix(expenses): enforce scoped category uniqueness

--- packages/db/drizzle/0005_expense_category_scope.sql
+++ packages/db/drizzle/0005_expense_category_scope.sql
@@ -1,2 +1,4 @@
 ALTER TABLE "expense_category" ADD COLUMN "organization_id" uuid REFERENCES "organization"("id") ON DELETE CASCADE;
+DROP INDEX IF EXISTS "expense_category_name_unique";
 CREATE INDEX "expense_category_organization_id_idx" ON "expense_category" ("organization_id");
+CREATE UNIQUE INDEX "expense_category_organization_id_name_uidx" ON "expense_category" ("organization_id","name");
