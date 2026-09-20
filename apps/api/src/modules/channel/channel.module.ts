644fe73f01448c36234908965a5b554d11b4cc6c
Daniel Mbazu <Mbazudaniel97@gmail.com>
2026-09-20 15:13:33 +0000

    feat(mvp): wire conversational module into channels

--- apps/api/src/modules/channel/channel.module.ts
+++ apps/api/src/modules/channel/channel.module.ts
@@ -2,10 +2,12 @@ import { Module } from "@nestjs/common";
 import { DbModule } from "../../common/db/db.module";
 import { ChannelAuthService } from "./channel-auth.service";
 import { ChannelController } from "./channel.controller";
+import { ConversationalModule } from "../conversational/conversational.module";
 import { ChannelInboundService } from "./channel-inbound.service";
 import { ChannelService } from "./channel.service";
 
 @Module({
+  imports: [ConversationalModule],
   imports: [DbModule],
   controllers: [ChannelController],
   providers: [ChannelService, ChannelAuthService, ChannelInboundService],
