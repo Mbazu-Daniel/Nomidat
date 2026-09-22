72d8d0a53848fe2464fb3551ba0d16d675625889
Daniel Mbazu <Mbazudaniel97@gmail.com>
2026-09-22 11:49:08 +0000

    fix: remove escaped newlines from database client

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -3,9 +3,14 @@ import postgres from "postgres";
 
 export type Database = PostgresJsDatabase<Record<string, never>>;
 
-export type DatabaseClient = Database & { close: () => Promise<void> };\n\nexport function createDb(connectionString: string): DatabaseClient {
+export type DatabaseClient = Database & { close: () => Promise<void> };
+
+export function createDb(connectionString: string): DatabaseClient {
   const client = postgres(connectionString);
-  const db = drizzle(client);\n  return Object.assign(db, {\n    close: () => client.end({ timeout: 5 }),\n  });
+  const db = drizzle(client);
+  return Object.assign(db, {
+    close: () => client.end({ timeout: 5 }),
+  });
 }
 
 export { generateId } from "./id";
